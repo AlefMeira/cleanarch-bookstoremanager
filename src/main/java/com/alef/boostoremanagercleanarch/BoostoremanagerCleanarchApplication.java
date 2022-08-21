e1eed3cb3b5346132f8a7c5b1518b9245ed01944
alefmeira <alef.chaves01@gmail.com>
2022-08-21 11:24:33 -0300

    first commit

---
+++ src/main/java/com/alef/boostoremanagercleanarch/BoostoremanagerCleanarchApplication.java
@@ -0,0 +1,13 @@
+package com.alef.boostoremanagercleanarch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoostoremanagerCleanarchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoostoremanagerCleanarchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alef/boostoremanagercleanarch/BoostoremanagerCleanarchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alef.boostoremanagercleanarch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoostoremanagerCleanarchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
